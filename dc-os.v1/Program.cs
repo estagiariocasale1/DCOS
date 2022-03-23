d2e2855d2da2e016974892ab09a434c4d6e1ed31
Daniel Carvalho <daniel.carvalho@ctec.ufal.br>
2022-03-23 14:23:17 -0300

    Adicionar arquivos de projeto.

---
+++ dc-os.v1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using dc_os.v1.dao;
+
+namespace dc_os.v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conexao conn = new Conexao();
+            conn.GetConexao();
+            
+
+
+           
+        }
+    }
+}
---
+++ dc-os.v1/dao/conexao.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+
+namespace dc_os.v1.dao
+{
+    
+}
