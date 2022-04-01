2b6a24dcf559c8d3bba782a886fd2a8e03bdcdca
Daniel Carvalho <daniel.carvalho@ctec.ufal.br>
2022-04-01 17:09:34 -0300

    Iniciada a View

--- dc-os.v1/Program.cs
+++ dc-os.v1/Program.cs
@@ -5,17 +5,20 @@ using System.Linq;
 using Deedle;
 using dc_os.v1.model.services;
 using Npgsql;
+using System.Windows.Forms;
+using view;
+
 
 namespace dc_os.v1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-
-             
-
-            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new view.MainMenu());
 
 
 
--- dc-os.v1/model/entidades/BoletimViagem.cs
+++ dc-os.v1/model/entidades/BoletimViagem.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace dc_os.v1.model.entidades{
     public class BoletimViagem {
